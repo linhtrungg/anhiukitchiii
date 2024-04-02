e27427723ec27923be6836ca5f22999f4a2f9b58
Trung Linh Nguyen <86712133+linhtrungg@users.noreply.github.com>
2024-04-02 21:39:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Kitchii àaa!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Bé có biết ấn zô đây hônggg',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là iu anh <33',
+    btnNo: 'Hong, em chỉ xem anh là anh trai thoiii :3',
+    question:'Trên thế giới gần 8 tỉ người mà sao em lại iu anh <3',
+    btnReply: 'Có biết gửi cho anh hônggg 🫶',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anh bic mò 🥰. Yêu bé nhìu nhìu 💕',
+    messDesc: 'Tối nay 7g anh qua đón công chúa đi chơi nhóeee.',
+    btnAccept: 'Được lunnnn <3',
+    messLink: 'https://m.me/TrungLinhNguyennn'
 }
