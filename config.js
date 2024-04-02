7cbcd5fe96ba44ac744f32bc150c78093a5e5cce
Trung Linh Nguyen <86712133+linhtrungg@users.noreply.github.com>
2024-04-02 21:46:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Kitchii àaa!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Trái đất vốn lạ thường,
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi.
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Bé có biết ấn zô đây hônggg',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là iu anh <33',
+    btnYes: 'Em iu lt nhìuuu <33',
     btnNo: 'Hong, em chỉ xem anh là anh trai thoiii :3',
     question:'Trên thế giới gần 8 tỉ người mà sao em lại iu anh <3',
     btnReply: 'Có biết gửi cho anh hônggg 🫶',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại vì lt là gu của em, em mê lt nhìu lắmmmmm ❤️',
     mess: 'Anh bic mò 🥰. Yêu bé nhìu nhìu 💕',
     messDesc: 'Tối nay 7g anh qua đón công chúa đi chơi nhóeee.',
     btnAccept: 'Được lunnnn <3',
