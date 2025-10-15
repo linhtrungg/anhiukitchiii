52b0338805a13805c92e4440670ab79a5c0bbe2f
Trung Linh Nguyen <86712133+linhtrungg@users.noreply.github.com>
2025-10-15 19:22:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Xuân Anh àaaaa!',
+    introTitle: 'Em Ốc iu àaaaa!',
     introDesc: `Trái đất vốn lạ thường,
     mà sao em cứ đi nhầm đường
     lạc vào tim anh lẻ loi.
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Ẻm có biết ấn zô đây hônggg',
+    btnIntro: 'Em có bic ấn zô đây hônggg',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Ẻm iu lt nhìuuu <33',
-    btnNo: 'Hong, em chỉ xem anh là anh trai thoiii :3',
-    question:'Trên thế giới gần 8 tỉ người mà sao em lại iu anh <3',
-    btnReply: 'Có biết gửi cho anh hônggg 🫶',
-    reply: 'Tại vì lt là gu của em, em mê lt dữ lắmmmmm ❤️',
-    mess: 'Ảnh bic mò 🥰. Yêu ẻm nhìu nhìu 💕',
+    btnYes: 'Em thích anh hìii <33',
+    btnNo: 'Hong, em chỉ xem anh là bạn thoiii :3',
+    question:'Trên thế giới gần 8 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Có biết gửi cho anh hong z 🫶',
+    reply: 'Tại vì anh là gu của em, em mê anh dữ lắm lun ❤️',
+    mess: 'Anh bic mò 🥰. Yêu em nhìu nhìu 💕',
     messDesc: 'Tối mai 7g anh qua đón công chúa đi chơi nhóeee.',
     btnAccept: 'Được lunnnn <3',
     messLink: 'https://m.me/TrungLinhNguyennn'
